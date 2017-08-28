4a1f2e2cd03450dc57f1070a532a3617b9813e80
Joshua Rudio <joshuarudio@gmail.com>
2017-08-28 11:42:23 -0700

    Reorder code and remove typo

--- src/Nav.js
+++ src/Nav.js
@@ -6,14 +6,8 @@ const Wrapper = styled.div`
   overflow: hidden;
   text-align: center;
   display: block;
+  opacity: 0.9;
 `;
-
-const Link = ({ className, children }) =>
-  <li className={className}>
-    <a href={`/${children}`}>
-      {children}
-    </a>
-  </li>;
 
 const StyledLink = styled(Link)`
   display: inline-block;
@@ -23,15 +17,22 @@ const StyledLink = styled(Link)`
   font-size: 17px;
 `;
 
+const Link = ({ className, children }) =>
+  <li className={className}>
+    <a href={`/${children.toLowerCase()}`}>
+      {children}
+    </a>
+  </li>;
+
 class Nav extends Component {
   render() {
     return (
       <Wrapper>
         <ul className="header-nav">
-          <StyledLink className="home" children="Home" />
-          <StyledLink className="about" children="About" />
-          <StyledLink className="projects" children="Projects" />
-          <StyledLink className="contact" children="Contact" />
+          <StyledLink className="home-link" children="Home" />
+          <StyledLink className="about-link" children="About" />
+          <StyledLink className="projects-link" children="Projects" />
+          <StyledLink className="contact-link" children="Contact" />
         </ul>
       </Wrapper>
     );
